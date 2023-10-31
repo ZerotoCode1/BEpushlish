9fa29238ac13ea63943f7e5a9cd159bc18b31caf
ZerotoCode1 <quang11102002f@gmail.com>
2023-10-31 14:30:32 +0700

    vn pay

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,8 +4,10 @@ import Order from '../models/orderModel.js';
 import User from '../models/userModel.js';
 import Product from '../models/productModel.js';
 import nodemail from "nodemailer"
+import dateFormat from"./dateFornat.js"
 import { isAuth, isAdmin, mailgun, payOrderEmailTemplate, payOrderEmailAdminTemplate } from '../utils.js';
-
+import  querystring from 'qs';
+import Crypto from "crypto"
 const orderRouter = express.Router();
 
 orderRouter.get(
@@ -223,4 +225,104 @@ orderRouter.delete(
   })
 );
 
+
+function formatDateToCustomString(date) {
+  const year = date.getFullYear().toString();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  
+  return year + month + day + hours + minutes + seconds;
+}
+
+function formatTimeToCustomString(date) {
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  
+  return hours + minutes + seconds;
+}
+
+function sortObject(obj) {
+	let sorted = {};
+	let str = [];
+	let key;
+	for (key in obj){
+		if (obj.hasOwnProperty(key)) {
+		str.push(encodeURIComponent(key));
+		}
+	}
+	str.sort();
+    for (key = 0; key < str.length; key++) {
+        sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
+    }
+    return sorted;
+}
+
+orderRouter.post(
+  '/create_payment_url',
+  expressAsyncHandler(async (req, res) => {
+    var ipAddr = req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
+var tmnCode = "1KZAJLXY";
+var secretKey = "NYKSNMPFGGVKACERLZFWEHZMXSPNNAGC";
+var vnpUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+var returnUrl = "http://localhost:3000/";
+
+var date = new Date();
+
+var createDate = formatDateToCustomString(date);
+var orderId = formatTimeToCustomString(date);
+var amount = req.body.vnorrder.totalPrice;
+var bankCode = req.body.bank;
+
+var orderInfo = "thanh toán tiền";
+var orderType = 100001;
+var locale = "";
+if(locale === null || locale === ''){
+    locale = 'vn';
+}
+var currCode = 'VND';
+var vnp_Params = {};
+vnp_Params['vnp_Version'] = '2.1.0';
+vnp_Params['vnp_Command'] = 'pay';
+vnp_Params['vnp_TmnCode'] = tmnCode;
+// vnp_Params['vnp_Merchant'] = ''
+vnp_Params['vnp_Locale'] = locale;
+vnp_Params['vnp_CurrCode'] = currCode;
+vnp_Params['vnp_TxnRef'] = orderId;
+vnp_Params['vnp_OrderInfo'] = orderInfo;
+vnp_Params['vnp_OrderType'] = orderType;
+vnp_Params['vnp_Amount'] = amount * 100;
+vnp_Params['vnp_ReturnUrl'] = returnUrl;
+vnp_Params['vnp_IpAddr'] = ipAddr;
+vnp_Params['vnp_CreateDate'] = createDate;
+if(bankCode !== null && bankCode !== ''){
+    vnp_Params['vnp_BankCode'] = bankCode;
+}
+
+
+try {
+  vnp_Params = sortObject(vnp_Params);
+
+
+var signData = querystring.stringify(vnp_Params, { encode: false });
+    
+var hmac = Crypto.createHmac("sha512", secretKey);
+var signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex"); 
+vnp_Params['vnp_SecureHash'] = signed;
+vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
+
+res.send(vnpUrl)
+
+} catch (error) {
+  console.log(error);
+}
+
+  })
+);
 export default orderRouter;
